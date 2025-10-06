ebf53a23c3b880d0a76803687e06503b43463b0e
Chase Thilleman <chasechaisson@me.com>
2025-10-06 16:07:50 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import Header from "../components/header";
 import Form from "../components/form";
 import ApplicationCard from "../components/applicationCard";
