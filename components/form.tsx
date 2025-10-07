4608e920df5b90d774b6082c447b257240f09d81
Chase Thilleman <chasechaisson@me.com>
2025-10-07 15:31:25 -0700

    Update form.tsx

--- components/form.tsx
+++ components/form.tsx
@@ -93,7 +93,7 @@ export default function Form({ addApplication }: FormProps) {
                 id="status"
                 value={formData.status}
                 onChange={onChange}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="appearance-none mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
                 {STATUS_OPTIONS.map((option) => (
                     <option key={option} value={option}>
