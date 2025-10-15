a9f08830079bb7f5a1079d5b527cf97a3809ca10
Chase Thilleman <chasechaisson@me.com>
2025-10-14 19:43:13 -0700

    Update applicationCard.tsx

--- components/applicationCard.tsx
+++ components/applicationCard.tsx
@@ -152,7 +152,7 @@ export default function ApplicationCard(props: ApplicationCardProps) {
                     />
                     {application.notes}
                 </p>
-                <p className="flex items-center py-1 break-all text-slate-600 dark:text-slate-300">
+                <p className="flex items-center py-3 break-all text-slate-600 dark:text-slate-300">
                     {props.link ? (
                         <a
                             href={props.link}
